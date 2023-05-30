da17b60684baf64c000941b3f54dc3e9607fb69e
DiegoCP17 <diego.cordoba@correo.tdea.edu.co>
2023-05-30 15:55:53 -0500

    insercion de Hooks

--- screens/Home.js
+++ screens/Home.js
@@ -1,26 +1,63 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import {
-    ActivityIndicator,
-    View,
-    StyleSheet,
-    Dimensions,
-    ScrollView,
-} from 'react-native';
+  ActivityIndicator,
+  View,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+} from "react-native";
+
+import {
+  getPopularMovies,
+  getUpcomingMovies,
+  getPopularTv,
+  getFamilyMovies,
+  getDocumentaryMovies,
+} from "../services/service";
+import { SliderBox } from "react-native-image-slider-box";
+
+const dimentions = Dimensions.get("screen");
+
+const Home = ({ navigation }) => {
+  {
+    /*Hooks para los estados de las categorias */
+  }
+  const [moviesImages, setMoviesImages] = useState();
+  const [popularMovies, setPopuarMovies] = useState();
+  const [popularTv, setPopularTv] = useState();
+  const [familyMovies, setFamilyMovies] = useState();
+  const [documentaryMovies, setDocumentaryMovies] = useState();
+  {
+    /*Hooks para el manejo de los errores y Carga */
+  }
+  const [error, setError] = useState();
+  const [loaded, setLoaded] = useState();
+
+  const getData = () => {
+      return Promise.all([
+        getPopularMovies(),
+        getUpcomingMovies(),
+        getPopularTv(),
+        getFamilyMovies(),
+        getDocumentaryMovies(),
+      ]);
+  };
+};
 
 const styles = StyleSheet.create({
-    sliderContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    sliderStyle: {
-        height: 0,
-    },
-    carousel: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+  sliderContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  sliderStyle: {
+    height: 0,
+  },
+  carousel: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
-export default Home
+export default Home;
