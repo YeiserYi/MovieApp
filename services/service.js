bcac6c415d7ced2f6e9fd8cb22fb9b104fc16b83
YeiserYi <Yeiser546@gmail.com>
2023-05-29 21:01:36 -0500

    Creando servicios api TMDB

--- services/service.js
+++ services/service.js
@@ -4,3 +4,51 @@ import axios from "axios";
 const apiUrl = 'https://api.themoviedb.org/3';
 //Falta apiKey de Diego
 const apiKey = '';
+
+//Obtener Categoria de Peliculas Populares
+export const  getPopularMovies = async () => {
+    const response = await axios.get(`${apiUrl}/movie/popular?${apiKey}`);
+    return response.data.results;
+};
+//Obtener Categoría próximas en películas / GET
+export const getUpcomingMovies = async () => {
+    const resp = await axios.get(`${apiUrl}/movie/upcoming?${apiKey}`);
+    return resp.data.results;
+};
+  
+//Obtener Categoría Programas Populares de TV / GET
+export const getPopularTv = async () => {
+const resp = await axios.get(`${apiUrl}/tv/popular?${apiKey}`);
+return resp.data.results;
+};
+
+//Obtener Categoría Familiar en películas / GET
+export const getFamilyMovies = async () => {
+const resp = await axios.get(
+    `${apiUrl}/discover/movie?${apiKey}&with_genres=10751`,
+);
+return resp.data.results;
+};
+
+//Obtener Categoría Documentales en películas / GET
+export const getDocumentaryMovies = async () => {
+const resp = await axios.get(
+    `${apiUrl}/discover/movie?${apiKey}&with_genres=99`,
+);
+return resp.data.results;
+};
+
+//obtener película por id
+export const getMovie = async id => {
+    const resp = await axios.get(`${apiUrl}/movie/${id}?${apiKey}`);
+    return resp.data;
+};
+  
+//Buscar contenidos por medio de una letra o palabra
+export const searchMovieTv = async (query, type) => {
+const resp = await axios.get(
+    `${apiUrl}/search/${type}?${apiKey}&query=${query}`,
+);
+return resp.data.results;
+};
+
