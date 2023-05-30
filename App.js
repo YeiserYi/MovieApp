c450d108d4cb2372d34d377427a949d2c7b79e5a
DiegoCP17 <diego.cordoba@correo.tdea.edu.co>
2023-05-30 17:28:12 -0500

    Cambios realizados correctamente

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import * as React from 'react';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import MainNavigation from "./components/MainNavigation";
+import "react-native-gesture-handler";
 
-const App = ()=> {
+const App = () => {
   return (
-   <></>
+    <NavigationContainer>
+      <MainNavigation />
+    </NavigationContainer>
   );
-}
+};
 
-export default App
-
+export default App;
